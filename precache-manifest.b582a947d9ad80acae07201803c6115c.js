7a27b34743db8a3d0a96cf178920c9293d6ab044
Brandon Carter <bcarteratx@gmail.com>
2020-04-14 10:55:23 -0500

    Updates

---
+++ precache-manifest.b582a947d9ad80acae07201803c6115c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383af1324405d4ccd467bd6fb431f81b",
+    "url": "/bcarteratx/project-4-assessment.git/index.html"
+  },
+  {
+    "revision": "789e5ed93ff7b1102116",
+    "url": "/bcarteratx/project-4-assessment.git/static/css/main.150830f0.chunk.css"
+  },
+  {
+    "revision": "0cb5e5cbe2fe2aade064",
+    "url": "/bcarteratx/project-4-assessment.git/static/js/2.bef04ba5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bcarteratx/project-4-assessment.git/static/js/2.bef04ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "789e5ed93ff7b1102116",
+    "url": "/bcarteratx/project-4-assessment.git/static/js/main.727a27a6.chunk.js"
+  },
+  {
+    "revision": "30609e6a45888d940d3a",
+    "url": "/bcarteratx/project-4-assessment.git/static/js/runtime-main.f1f89937.js"
+  }
+]);
